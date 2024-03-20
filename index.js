79d4c566cce5219b12833034bc5e02b29f49f256
Surajit Maity <sibu23122003@gmail.com>
2024-03-20 16:28:26 +0530

    mongodb atlas

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ main().then(() => {
   .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/portfolio');
+  await mongoose.connect('mongodb+srv://sibu23122003:ddxRdUouTqfr6koe@cluster0.7zcjfgf.mongodb.net/portfolio');
   
 }
 //Home Route
